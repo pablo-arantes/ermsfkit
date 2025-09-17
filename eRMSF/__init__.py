12733df8d30241c96329e08205df1e3e6b22d635
Pablo Arantes <pabloarantes@Pablos-MacBook-Pro.local>
2025-09-17 15:25:40 -0300

    add class

--- eRMSF/__init__.py
+++ eRMSF/__init__.py
@@ -1,9 +1,11 @@
 """
-eRMSF
-eRMSF: A Python Package for Ensemble RMSF Analysis of Molecular Dynamics and Structural Ensembles
+ermsf
+A package to perform time-dependent RMSF analysis on molecular dynamics data.
 """
 
 # Add imports here
+from .ermsf import ermsf
+
 from importlib.metadata import version
 
-__version__ = version("ermsfkit")
+__version__ = version("ermsf")
